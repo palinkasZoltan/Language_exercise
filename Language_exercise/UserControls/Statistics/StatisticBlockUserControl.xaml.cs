10058648c1a3b56a3f50fc9341af8238c1f79f31
palinkasZoltan <77148226+palinkasZoltan@users.noreply.github.com>
2022-09-19 13:10:33 +0200

    Refactor statistics view

---
+++ Language_exercise/UserControls/Statistics/StatisticBlockUserControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="StatisticBlockUserControl.xaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Language_exercise.UserControls.Statistics
+{
+    using System.ComponentModel;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for StatisticBlockUserControl.xaml.
+    /// </summary>
+    public partial class StatisticBlockUserControl : UserControl
+    {
+        public StatisticBlockUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
